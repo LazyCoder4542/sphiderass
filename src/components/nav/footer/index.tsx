660f5bc48087ae5cf16c7b98679453b8f3ae3573
Adeola Fadare <adeolafadare8@gmail.com>
2024-12-15 15:37:28 +0100

    add: social links

--- src/components/nav/footer/index.tsx
+++ src/components/nav/footer/index.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
 import { useTheme } from "../../../context/theme";
 import styles from "./style.module.css";
+
 function Footer() {
-  const {mode} = useTheme()
+  const { mode } = useTheme();
   return (
     <footer id={styles["site-footer"]}>
       <div className={styles.main}>
@@ -20,10 +21,18 @@ function Footer() {
           <div>
             <span>QUICK LINKS</span>
             <ul>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/services">Services</Link></li>
-              <li><Link to="/blogs">Blogs</Link></li>
-              <li><Link to="/jobs#openings">Career</Link></li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/services">Services</Link>
+              </li>
+              <li>
+                <Link to="/blogs">Blogs</Link>
+              </li>
+              <li>
+                <Link to="/jobs#openings">Career</Link>
+              </li>
             </ul>
           </div>
           <div>
@@ -48,30 +57,54 @@ function Footer() {
           </div>
         </div>
         <div className={styles.socials}>
-          <div>
-            <span>
-              <img src="/icons/social/facebook.svg" alt="facebook" />
-            </span>
-            <span>Facebook</span>
-          </div>
-          <div>
-            <span>
-              <img src="/icons/social/linkedin.svg" alt="linkedin" />
-            </span>
-            <span>Linkedin</span>
-          </div>
-          <div>
-            <span>
-              <img src="/icons/social/twitter.svg" alt="twitter" />
-            </span>
-            <span>Twitter</span>
-          </div>
-          <div>
-            <span>
-              <img src="/icons/social/instagram.svg" alt="instagram" />
-            </span>
-            <span>Instagram</span>
-          </div>
+          <Link
+            to="https://www.facebook.com/share/Qw3P2XNMph7rz8NV/?mibextid=LQQJ4d"
+            target="_blank"
+            referrerPolicy="no-referrer"
+          >
+            <div>
+              <span>
+                <img src="/icons/social/facebook.svg" alt="facebook" />
+              </span>
+              <span>Facebook</span>
+            </div>
+          </Link>
+          <Link
+            to="https://www.linkedin.com/company/sphiderass-web-ltd"
+            target="_blank"
+            referrerPolicy="no-referrer"
+          >
+            <div>
+              <span>
+                <img src="/icons/social/linkedin.svg" alt="linkedin" />
+              </span>
+              <span>Linkedin</span>
+            </div>
+          </Link>
+          <Link
+            to="https://x.com/Sphiderassweb"
+            target="_blank"
+            referrerPolicy="no-referrer"
+          >
+            <div>
+              <span>
+                <img src="/icons/social/twitter.svg" alt="twitter" />
+              </span>
+              <span>Twitter</span>
+            </div>
+          </Link>
+          <Link
+            to="https://www.instagram.com/sphiderassweb/?igsh=NXcyczJzeHNlcnk2"
+            target="_blank"
+            referrerPolicy="no-referrer"
+          >
+            <div>
+              <span>
+                <img src="/icons/social/instagram.svg" alt="instagram" />
+              </span>
+              <span>Instagram</span>
+            </div>
+          </Link>
         </div>
       </div>
       <span>© 2024 - Sphiderassweb.Ltd</span>
@@ -85,7 +118,9 @@ function ThemeToggler() {
   const { mode, toggleMode } = useTheme();
   return (
     <div className={styles.toggle} data-val={mode == "dark" ? 1 : 0}>
-      <span className={mode == "light" ? styles.active : undefined}>Light Mode</span>
+      <span className={mode == "light" ? styles.active : undefined}>
+        Light Mode
+      </span>
       <div
         onClick={() => {
           toggleMode(mode == "dark" ? "light" : "dark");
@@ -95,7 +130,9 @@ function ThemeToggler() {
           <span className={styles.ball}></span>
         </div>
       </div>
-      <span className={mode == "dark" ? styles.active : undefined}>Dark Mode</span>
+      <span className={mode == "dark" ? styles.active : undefined}>
+        Dark Mode
+      </span>
     </div>
   );
 }
